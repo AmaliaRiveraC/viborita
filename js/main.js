4ba22ef26d8aa5bdc42411b894c39efc0fb92432
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-04-06 15:45:08 +0000

    declarando variables

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var contenedor = document.getElementById("contenedor");
+var vibora = document.getElementById("vibora");
+var posY = 0;
+var posX = 0;
+var movimiento = 20;
+
+var teclas = {
+    left: 37,
+    right: 30,
+    up: 38,
+    down: 39
+};
+
+vibora.addEventListener("keydown", avanzar);
+
+function avanzar(evento.keyCode){
+    switch(teclas)={
+        case left:
+            
+            break;
+        case right:
+            break;
+        case up:
+            break;
+        case down:
+            break;
+        default:
+            break;
+            
+            
+                 }
+}
+
+function finDelJuego(){
+    
+}
