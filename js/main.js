d601226cafb83b7d6fd234622d6a1991d7185c5e
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-04-09 17:12:39 -0500

    mejorando proporciones y agregando funcion reiniciar

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 document.addEventListener("keydown", avanzar);
 var vibora = document.getElementById("vibora");
+var boton = document.getElementById("reiniciar");
+boton.addEventListener("click", reIniciar);
+
+//Posiciones iniciales según el css margin-right y margin-left "auto"
 var posY = 0;
 var posX = 0;
-var movimiento = 21;
+
+//Unidad de movimietno que le suma uno a la medida de la vibora para avanzar según la medida de la vibora
+var movimiento = 50;
 
 
 
 function avanzar(evento){
+    /*Función que controla el movimiento de la vibora con un evento que detecta cuando se oprimen las teclas flechas 
+     y en función de que flecha se oprime la vibora toma una dirección*/
 
     switch(evento.keyCode) {
         case 38: //Arriba
@@ -56,7 +64,17 @@ function avanzar(evento){
                  }
 }
 
-function juegoAcabado(){
+function juegoAcabado(){ 
+    /*funcion que detiene el juego cuando la vibora toca alguno de los límites del contenedor*/ 
     alert("Game over");
     document.removeEventListener("keydown", avanzar);
+}
+
+function reIniciar(event) {
+    
+    vibora.style.marginTop = 0;
+    vibora.style.marginLeft = 0;
+    document.removeEventListener("keydown", avanzar);
+    document.addEventListener("keydown", avanzar)
+    console.log(event);
 }
